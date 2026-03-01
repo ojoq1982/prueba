e03183a71f0f949d7aee276099b6390646dcf870
ojoq1982 <ojoq1982@gmail.com>
2026-02-28 21:18:22 -0500

    Add files via upload

---
+++ coros-pwa/songs.js
@@ -0,0 +1,37 @@
+const songs = [
+  {
+    titulo: "POBRE PEREGRINO",
+    letra: `Pobre peregrino que vagando vas,
+Cristo es el camino, Cristo es la verdad.
+Si te encuentras débil fuerzas te dará,
+Si estás oprimido, te libertará.
+
+Para los muertos hay vida,
+Para los pobres riqueza,
+Para los ciegos hay luz,
+De Jesús, para ti.`
+  },
+  {
+    titulo: "ALABAD, BENDECID A JEHOVÁ",
+    letra: `Alabad, bendecid a Jehová,
+Vosotros los hijos de Dios. (Bis)
+Alzad vuestras manos, batidlas a Él
+Y decidle que solo Él es fiel. (Bis)`
+  },
+  {
+    titulo: "ALABA ALMA MÍA A JEHOVÁ",
+    letra: `Alaba alma mía a Jehová. (Bis)
+Alabaré a Jehová en mi vida mientras viva.
+Jehová liberta los cautivos,
+Jehová da vista a los ciegos,
+Jehová levanta los caídos,
+Jehová ama a los justos.`
+  },
+  {
+    titulo: "SOY FELIZ",
+    letra: `Soy feliz, Cristo me salvó.
+Soy feliz, Él me libertó.
+Soy feliz, Él me bautizó.
+Canto gloria, aleluya, Él me libertó.`
+  }
+];
